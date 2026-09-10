447b556bf8e9fece09d1e890a4caac8f2d80027b
Arithmetic-Power-Geometry <akhtarneha310@gmail.com>
2026-09-10 21:23:06 +0530

    Test escaped authoritative Health OGD payload evidence

---
+++ tests/test_health_ogd_resolver_escapes.py
@@ -0,0 +1,40 @@
+from modules.health_access.resolve_ogd_hospital_resource_identity import (
+    _explicit_official_payload_urls,
+    _explicit_resource_url_uuids,
+)
+
+
+def test_decodes_escaped_official_resource_uuid_without_constructing_it():
+    rid = "123e4567-e89b-42d3-a456-426614174000"
+    text = (
+        'window.__DATA__={"url":"https:\\u002F\\u002Fapi.data.gov.in'
+        f'\\u002Fresource\\u002F{rid}"}}'
+    )
+    assert _explicit_resource_url_uuids(text) == [rid]
+
+
+def test_decodes_escaped_official_csv_url():
+    text = (
+        'window.__DATA__={"download":"https:\\/\\/www.data.gov.in\\/files\\/'
+        'hospital_directory.csv"}'
+    )
+    assert _explicit_official_payload_urls(text) == [
+        "https://www.data.gov.in/files/hospital_directory.csv"
+    ]
+
+
+def test_rejects_escaped_third_party_payload_url():
+    text = (
+        'window.__DATA__={"download":"https:\\u002F\\u002Fexample.org'
+        '\\u002Fhospital_directory.csv"}'
+    )
+    assert _explicit_official_payload_urls(text) == []
+
+
+def test_catalog_uuid_is_still_excluded_after_escape_normalization():
+    catalog_id = "e48a8bcf-ff56-4f39-839d-095827ba2a18"
+    text = (
+        'window.__DATA__={"url":"https:\\u002F\\u002Fapi.data.gov.in'
+        f'\\u002Fresource\\u002F{catalog_id}"}}'
+    )
+    assert _explicit_resource_url_uuids(text) == []
